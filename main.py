f43ab457c4a367234b3607e55805e1fc95601432
fartuhmarko <145208478+fartuhmarko@users.noreply.github.com>
2024-02-24 17:21:45 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,15 +8,41 @@ class GameSprite():
         self.speed = speed
         self.rect.x = x
         self.rect.y = y
-
+        self.direction = "right"
         self.move_left = False
 
     def reset(self):
-        
-        if self.move_left == True:
-            self.rect.x += self.speed
-
         window.blit(self.img, (self.rect.x, self.rect.y))
+
+class Hero(GameSprite):
+    def move(self):
+        keys = key.get_pressed()
+        if keys [K_d]:
+            if self.rect.x < 640:
+                self.rect.x +=self.speed
+        if keys [K_a]:
+            if self.rect.x > 5:
+                self.rect.x -= self.speed
+                
+        if keys [K_w]:
+            if self.rect.y > 5:
+                self.rect.y -= self.speed
+        if keys [K_s]:
+            if self.rect.y < 440:
+                self.rect.y += self.speed
+            
+class Enemy(GameSprite):
+    def move(self, start, end):
+        if self.direction == "right":
+            self.rect.x += 5
+        if self.direction == "left":
+            self.rect.x -= 5
+        if self.rect.x >= end:
+            self.direction = "left"
+        if self.rect.x <= start:
+            self.direction = "left"
+        
+    
 
 window = display.set_mode((700,500))
 display.set_caption("Доганялки")
@@ -38,6 +64,7 @@ sprite2 = transform.scale(
 window.blit(sprite1, (x2, y2))
 clock = time.Clock
 
+enemy = Enemy("cyborg.png", 10, 50, 50)
 hero = GameSprite("hero.png", 10, 50, 50)
 mixer.init()
 mixer.music.load("jungles.ogg")
@@ -51,14 +78,12 @@ while game:
     window.blit(background,(0, 0))
     
     hero.reset()
+    hero.move()
+    enemy.reset()
+    enemy.move(300, 600)
     for e in event.get():
         if e.type == QUIT:
             game = False
-        if e.type == KEYDOWN:
-            if e.key == K_1:
-                kick.play()
-            if e.key == K_d:
-                hero.move_left = False
             
     keys_pressed = key.get_pressed()
         
